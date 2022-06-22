952334c784976d3332a78f3f9d067010fb518fbe
Victor <victor@Victors-PC.local>
2022-06-22 17:21:13 -0400

    added about container height

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,18 @@
-import styles from './About.module.css';
-import AboutContent from './AboutContent';
-import headshot from '../../media/headshot.jpeg'
+import styles from "./About.module.css";
+import AboutContent from "./AboutContent";
+import headshot from "../../media/headshot.jpeg";
 
 const About = () => {
-    return (
-        <div className={styles.container}>
-            <div><img className={styles.imageContainer} src={headshot}></img></div>
-            <div className={styles.headerContainer}>About me!<div className={styles.containerSmall}>{AboutContent}</div></div>
-            
-            
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div>
+        <img className={styles.imageContainer} src={headshot}></img>
+      </div>
+      <div className={styles.headerContainer}>
+        About me!<div className={styles.containerSmall}>{AboutContent}</div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
