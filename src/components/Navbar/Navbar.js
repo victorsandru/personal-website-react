cb0508fdb48cd827a957921202409ce46fd9ddd6
Victor <vsandru4@gmail.com>
2022-06-29 14:19:29 -0400

    added resume link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,35 +1,37 @@
 import styles from "./Navbar.module.css";
 import NavbarIcons from "./NavbarIcons";
-import NavbarSections from "./NavbarSections";
 
-const NAV_ITEMS = [
-  {
-    name: "About",
-    id: "about",
-  },
-  {
-    name: "Portfolio",
-    id: "portfolio",
-  },
-  {
-    name: "Resume",
-    id: "resume",
-  },
-];
+import { Link, animateScroll as scroll } from "react-scroll";
+
+import resume from "./CV.pdf";
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>
-      {NAV_ITEMS.map((item) => (
-        <NavbarSections
+    <>
+      <div className={styles.container}>
+        <Link className={styles.item} to="about" offset={-150} smooth={true}>
+          About
+        </Link>
+        <Link
           className={styles.item}
-          name={item.name}
-          key={item.id}
-          id={item.id}
-        />
-      ))}
-      <NavbarIcons className={styles.icon} />
-    </div>
+          to="portfolio"
+          offset={-150}
+          smooth={true}
+        >
+          Portfolio
+        </Link>
+        <a
+          className={styles.item}
+          id="resume"
+          href={resume}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Resume
+        </a>
+        <NavbarIcons className={styles.icon} />
+      </div>
+    </>
   );
 };
 
--- src/components/Navbar/NavbarSections.js
+++
@@ -1,9 +0,0 @@
-import { Link, animateScroll as scroll } from "react-scroll";
-
-const NavbarSections = props => {
-    return (
-        <Link className={props.className} to={props.id} offset={-150} smooth={true}>{props.name}</Link>
-    );
-}
-
-export default NavbarSections;
